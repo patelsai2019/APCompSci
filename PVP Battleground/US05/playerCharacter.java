6ebac892f333085c587f5773bd911d922ee90672
patelsai2019 <42677872+patelsai2019@users.noreply.github.com>
2019-03-21 22:40:08 -0700

    Create playerCharacter.java

---
+++ PVP Battleground/US05/playerCharacter.java
@@ -0,0 +1,23 @@
+public abstract class PlayerCharacter {
+
+    protected String name;
+
+    protected int strength;
+    protected int intelligence;
+    protected int agility;
+
+    protected int hp, maxhp;
+    protected int mp, maxmp;
+
+    public PlayerCharacter() {
+
+        System.out.println("A new PlayerCharacter!");
+
+    }
+
+    protected void showStats() {
+        System.out.printf(" HP: %3d/%3d\n", hp, maxhp);
+        System.out.printf(" MP: %3d/%3d\n", mp, maxmp);
+        System.out.printf("STR: %3d\nINT: %3d\nAGI: %3d\n",strength,intelligence,agility);
+    }
+}
