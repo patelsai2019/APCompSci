3643deafcdb75cd20095a19c294c1b2b7f88db7c
patelsai2019 <42677872+patelsai2019@users.noreply.github.com>
2019-03-21 22:29:38 -0700

    Create fighter.java

---
+++ PVP Battleground/US03-04/fighter.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+
+public class Fighter extends PlayerCharacter {
+
+    int constitution;
+
+    public Fighter(String newName) {
+
+        System.out.println("A new Fighter has been created!");
+
+        name = newName;
+
+        Random rand = new Random();
+
+        strength = 15 + (rand.nextInt(6) + 1);
+        intelligence = 7 + (rand.nextInt(6) + 1);
+        agility = 8 + (rand.nextInt(6) + 1);
+        constitution = 10 + (rand.nextInt(6) + 1);
+
+        hp = maxhp = (strength * 2) + (constitution * 2);
+        mp = maxmp = 0;
+
+    }
+
+    public void showStats() {
+        System.out.printf("%s, a Fighter:\n",name);
+        super.showStats();
+        System.out.printf("CON: %3d\n",constitution);
+    }
+}
