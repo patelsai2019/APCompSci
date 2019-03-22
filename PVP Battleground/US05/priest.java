15ecc9fa179b9b1e13d614881d93f01f6dd32856
patelsai2019 <42677872+patelsai2019@users.noreply.github.com>
2019-03-21 22:40:41 -0700

    Create priest.java

---
+++ PVP Battleground/US05/priest.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+
+public class Priest extends PlayerCharacter {
+
+    private int charisma;
+
+    public Priest(String newName) {
+
+        System.out.println("A new Paladin has been created!");
+
+        name = newName;
+
+        Random rand = new Random();
+
+        strength = 7 + (rand.nextInt(6) + 1);
+        intelligence = 15 + (rand.nextInt(6) + 1);
+        agility = 8 + (rand.nextInt(6) + 1);
+        charisma = 10 + (rand.nextInt(6) + 1);
+
+        hp = maxhp = strength;
+        mp = maxmp = intelligence + (2 * charisma);
+
+    }
+
+    public void showStats() {
+        System.out.printf("%s, a Priest:\n",name);
+        super.showStats();
+        System.out.printf("CHA: %3d\n",charisma);
+    }
+}
