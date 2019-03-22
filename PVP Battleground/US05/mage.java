47816eb891f3efb935f16bb6a788cca98a8a853d
patelsai2019 <42677872+patelsai2019@users.noreply.github.com>
2019-03-21 22:38:30 -0700

    Create mage.java

---
+++ PVP Battleground/US05/mage.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+
+public class Mage extends PlayerCharacter {
+
+    private int wisdom;
+
+    public Mage(String newName) {
+
+        System.out.println("A new Mage has been created!");
+
+        name = newName;
+
+        Random rand = new Random();
+
+        strength = 7 + (rand.nextInt(6) + 1);
+        intelligence = 15 + (rand.nextInt(6) + 1);
+        agility = 8 + (rand.nextInt(6) + 1);
+        wisdom = 10 + (rand.nextInt(6) + 1);
+
+        hp = maxhp = strength;
+        mp = maxmp = intelligence + (2 * wisdom);
+
+    }
+
+    public void showStats() {
+        System.out.printf("%s, a Mage:\n",name);
+        super.showStats();
+        System.out.printf("WIS: %3d\n",wisdom);
+    }
+}
